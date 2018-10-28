d38348b23da012b63f1219311761558290148dbb
Naga Sai Manoj Giddaluru <nagasai.g9@gmail.com>
2018-10-28 18:09:27 +0530

    code for pop-up window added

--- load_pics.js
+++ load_pics.js
@@ -1,30 +1,16 @@
-function full_screen_preview(url) {
-    $('html').html('<img style="max-width:100%; max-height:100%; object-fit: none" src="' + url + '" />')
-}
-
 function load_images() {
-    var folder = "photos/";
+    var folder = 'photos/';
 
     $.ajax({
         url: folder,
         success: function (data) {
             var i = 0;
-            $(data).find("a").attr("href", function (i, val) {
+            $(data).find('a').attr('href', function (i, val) {
                 if (val.match(/\.(jpe?g|png|gif)$/)) {
-                    switch (i % 4) {
-                        case 0:
-                            $("#col1").append("<img onclick=full_screen_preview('" + val + "') src='" + val + "'>");
-                            break;
-                        case 1:
-                            $("#col2").append("<img onclick=full_screen_preview('" + val + "') src='" + val + "'>");
-                            break;
-                        case 2:
-                            $("#col3").append("<img onclick=full_screen_preview('" + val + "') src='" + val + "'>");
-                            break;
-                        case 3:
-                            $("#col4").append("<img onclick=full_screen_preview('" + val + "') src='" + val + "'>");
-                            break;
-                    }
+                    var col = '#col' + (i % 4);
+
+                    $(col).append('<img class="each_img" src="' + val + '" >');
+
                     i++;
                 }
             });
@@ -32,6 +18,28 @@ function load_images() {
     });
 }
 
-$(document).ready(function () {
-    load_images();
-});
+function load_model() {
+    var modal = document.getElementById('myModal');
+    var modalImg = document.getElementById('img01');
+
+    $('.each_img').click(
+        function () {
+            modal.style.display = 'block';
+            modalImg.src = this.src;
+        }
+    );
+
+    $('.close').click(
+        function () {
+            modal.style.display = 'none';
+        }
+    );
+}
+
+$(
+    function () {
+        load_images();
+
+        load_model();
+    }
+);
